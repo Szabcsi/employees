5f23aca683d5b90309721156f3042eebe33650ec
Bekő Szabolcs <beko.szabolcs@gmail.com>
2021-09-03 14:13:10 +0200

    Validation error message format, according RFC 7807

--- src/main/java/employees/EmployeesApplication.java
+++ src/main/java/employees/EmployeesApplication.java
@@ -23,9 +23,9 @@ public class EmployeesApplication {
 		return new ModelMapper();
 	}
 
-	/*@Bean
+	@Bean
 	public ObjectMapper objectMapper(){
 		return new ObjectMapper().findAndRegisterModules();
-	}*/
+	}
 
 }
